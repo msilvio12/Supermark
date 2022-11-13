aa47a5f374b962727ef9d2573000f01f6eb17a94
msilvio12 <msilvio_12@outlook.com>
2022-11-13 00:55:39 -0300

    prueba
    Signed-off-by: msilvio12 <msilvio_12@outlook.com>

--- src/sql/main.py
+++ src/sql/main.py
@@ -2,5 +2,5 @@ import database
 import table
 
 if __name__ == '__main__':
-    db = database.Connection('Supermark\src\database\Supermark.sqlite3')
+    db = database.Connection('src\database\Supermark.sqlite3')
     table = table.Table.user_table('usuarios','')
