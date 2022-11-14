8d1621ca78da3eaf1467f3fde92a9df776029863
Kevin <Kevin_NDelgado11@outlook.com>
2022-11-13 22:00:46 -0300

    Test main

--- src/package/shopping_cart.py
+++ src/package/shopping_cart.py
@@ -2,12 +2,12 @@ import product
 
 class Carrito():
 
-    def __init__(self, producto: product.Producto):
+    def __init__(self):
             self.limite = 30
-            self.mi_carrito = [producto]
+            self.mi_carrito = []
 
     #Metodo para agregar objetos Producto al carrito usando diccionario
-    def agregar(self, item, cantidad):
+    def agregar(self, item: product.Producto, cantidad):
         if item not in self.mi_carrito:
             self.mi_carrito.append(item)
         return self.mi_carrito
